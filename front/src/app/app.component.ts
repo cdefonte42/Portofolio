e801ab272920b6b4ed84227197a0f2a96f51170a
Cyrielle DEFONTE <cyrielledefonte@gmail.com>
2024-05-15 16:45:59 +0200

    [add] incorporate nav bar and overview in app

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
-  // template: `<div>HEHEHE</div>`,
+  imports: [RouterOutlet, HeaderComponent, NavBarComponent, OverviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Portofolio';
+
 }
