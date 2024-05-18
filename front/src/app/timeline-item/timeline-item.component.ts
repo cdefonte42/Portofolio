3bfbdb6b68b1715ff2fa82642a0769644a5be941
Cyrielle DEFONTE <cyrielledefonte@gmail.com>
2024-05-18 14:53:59 +0200

    [add] timeline expansion animation

--- front/src/app/timeline-item/timeline-item.component.ts
+++ front/src/app/timeline-item/timeline-item.component.ts
@@ -1,22 +1,31 @@
 import { Component, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 import { Experience } from '../types/experience';
 import { NgIf } from '@angular/common';
+import { AUTO_STYLE, animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-timeline-item',
   standalone: true,
   imports: [NgIf],
   templateUrl: './timeline-item.component.html',
-  styleUrl: './timeline-item.component.css'
+  styleUrl: './timeline-item.component.css',
+  animations: [
+    trigger('collapse', [
+      state('open', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+      state('closed', style({ height: '0', visibility: 'hidden' })),
+      transition('closed => open', animate('500ms ease-in')),
+      transition('open => closed', animate('300ms ease-out'))
+    ])
+  ]
 })
 export class TimelineItemComponent {
   @Input({ required: true }) experience!: Experience;
   @Input() expanded: boolean = false;
-  @Output() expandedChange = new EventEmitter<Event>();
+  @Output() expandedChange = new EventEmitter<boolean>();
 
-  setExpand(val: boolean, event: Event) {
+  setExpand(val: boolean) {
     this.expanded = val;
-    this.expandedChange.emit(event);
+    this.expandedChange.emit(val);
   }
 
 }
