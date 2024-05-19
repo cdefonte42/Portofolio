748c08abb84a036d5e582348b76da856578237c6
Cyrielle DEFONTE <cyrielledefonte@gmail.com>
2024-05-19 15:56:21 +0200

    [add] sticky header and some styles

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { LayoutComponent } from './layout/layout.component';
 import { OverviewComponent } from './overview/overview.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { SectionTitleComponent } from './section-title/section-title.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LayoutComponent, OverviewComponent, TimelineComponent, SectionTitleComponent],
+  imports: [LayoutComponent, OverviewComponent, TimelineComponent, SectionTitleComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
