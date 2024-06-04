28e70440ca38d0980c743ca020fdb4bbf4a40d4a
Cyrielle DEFONTE <cyrielledefonte@gmail.com>
2024-06-04 12:25:59 +0200

    [add] navigation click scrollInto

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -7,20 +7,24 @@ import { CardComponent } from './card/card.component';
 import { ProjectComponent } from './project/project.component';
 import { Projects } from './datas/project-data';
 import { Project } from './types/project';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, ProjectComponent, CardComponent, OverviewComponent, TimelineComponent, SectionTitleComponent, HeaderComponent],
+  imports: [NgIf, NgFor, ProjectComponent, CardComponent, OverviewComponent, TimelineComponent, SectionTitleComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+
 })
 export class AppComponent implements AfterViewInit {
   title = 'Cyrielle DEFONTE';
   projects: Project[] = Projects;
-  sections: string[] = ["CV", "EXPERIENCES", "SKILLS", "PROJETS"];
+
+  sections: string[] = window.screen.width > 860 ? ["CV", "PROJETS"] : ["CV", "EXPERIENCES", "SKILLS", "PROJETS"];
   currentSection?: string;
+  showNav: boolean = false;
+  scrollTarget: string | undefined = undefined;
 
   @ViewChild('cvRef', { read: ElementRef }) cvRef!: ElementRef;
   @ViewChild('timelineRef', { read: ElementRef }) timelineRef!: ElementRef;
@@ -28,9 +32,31 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('projectsRef', { read: ElementRef }) projectsRef!: ElementRef;
   @ViewChild('overviewRef', { read: ElementRef }) overviewRef!: ElementRef;
 
+  scrollIntoSection(section: string, event: Event) {
+    if (this.showNav)
+      event.stopPropagation();
+    const target = this.mapSectionToTarget(section);
+    if (!target) return;
+    this.scrollTarget = section;
+    target.target.nativeElement.scrollIntoView({ behavior: 'smooth', block: target.block, inline: 'start' });
+    this.showNav = false;
+  }
+
+  mapSectionToTarget(section: string) {
+    switch (section) {
+      case 'CV':
+        return { target: this.overviewRef, block: 'start' };
+      case 'EXPERIENCES':
+        return { target: this.timelineRef, block: 'center' };
+      case 'SKILLS':
+        return { target: this.skillsRef, block: 'center' };
+      case 'PROJETS':
+        return { target: this.projectsRef, block: 'start' };
+    }
+    return undefined;
+  }
+
   ngAfterViewInit() {
-    if (getComputedStyle(this.overviewRef.nativeElement).display === 'flex')
-      this.sections = ["CV", "PROJETS"];
 
     const options = {
       root: null,
@@ -54,16 +80,28 @@ export class AppComponent implements AfterViewInit {
   updateCurrentSection(element: Element) {
     switch (element) {
       case this.cvRef.nativeElement:
-        this.currentSection = this.sections[0];
+        if (this.scrollTarget === undefined || this.scrollTarget === this.sections[0]) {
+          this.currentSection = this.sections[0];
+          this.scrollTarget = undefined;
+        }
         break;
       case this.timelineRef.nativeElement:
-        this.currentSection = this.sections.length > 2 ? this.sections[1] : this.sections[0];
+        if (this.scrollTarget === undefined || this.scrollTarget === (this.sections.length > 2 ? this.sections[1] : this.sections[0])) {
+          this.currentSection = this.sections.length > 2 ? this.sections[1] : this.sections[0];
+          this.scrollTarget = undefined;
+        }
         break;
       case this.skillsRef.nativeElement:
-        this.currentSection = this.sections.length > 2 ? this.sections[2] : this.sections[0];
+        if (this.scrollTarget === undefined || this.scrollTarget === (this.sections.length > 2 ? this.sections[2] : this.sections[0])) {
+          this.currentSection = this.sections.length > 2 ? this.sections[2] : this.sections[0];
+          this.scrollTarget = undefined;
+        }
         break;
       case this.projectsRef.nativeElement:
-        this.currentSection = this.sections.length > 2 ? this.sections[3] : this.sections[1];
+        if (this.scrollTarget === undefined || this.scrollTarget === (this.sections.length > 2 ? this.sections[3] : this.sections[1])) {
+          this.currentSection = this.sections.length > 2 ? this.sections[3] : this.sections[1];
+          this.scrollTarget = undefined;
+        }
         break;
     }
   }
